312cc2a1a6c75e582c32001927359c97024963eb
Sergey Nefedov <nefedov38@gmail.com>
2024-10-30 19:57:55 +0300

    1

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,51 @@
+import bcrypt from "bcrypt";
+import models from "../models/models.js";
+import jwt from "jsonwebtoken";
 import ApiError from "../error/ApiError.js";
 
+const generateJwt = (id, email, role) =>
+  jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+
 class UserController {
-  async registation(req, res) {}
-  async login(req, res) {}
-  async checkAuth(req, res, next) {
-    const { id } = req.query;
-    if (!id) {
-      return next(ApiError.badRequest("asads"));
+  async registation(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest("Не корректный email или пароль"));
     }
-    res.json(id);
+    const candidate = await models.User.findOne({ where: { email } });
+    if (candidate) {
+      return next(
+        ApiError.badRequest("Пользователь с таким email уже существует")
+      );
+    }
+    const hashPassword = await bcrypt.hash(password, 5);
+    const user = await models.User.create({
+      email,
+      role,
+      password: hashPassword,
+    });
+    const basket = await models.Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    const user = await models.User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.badRequest("Такого пользователя не существует"));
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.badRequest("Указан не верный пароль"));
+    }
+    const token = generateJwt(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+  async checkAuth(req, res, next) {
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
   }
 }
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,10 @@
 import Router from "express";
 import userController from "../controllers/userController.js";
-
+import authMiddleware from "../middleware/authMiddleware.js";
 const router = new Router();
 
 router.post("/registration", userController.registation);
 router.post("/login", userController.login);
-router.get("/auth", userController.checkAuth);
+router.get("/auth", authMiddleware, userController.checkAuth);
 
 export default router;
